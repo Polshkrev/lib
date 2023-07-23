8762c220c81e961605c5089ce340cc5de6119603
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-07-23 13:36:33 -0400

    add implementation check

--- c/strings.h
+++ c/strings.h
@@ -30,3 +30,9 @@ string_t string_trim(string_t *string);
 size_t string_find_last_of(string_t *string, char_t charactor);
 
 #endif // STRINGS_H_
+
+#ifdef STRINGS_IMPLEMENTATION
+
+
+
+#endif // STRINGS_IMPLEMENTATION
