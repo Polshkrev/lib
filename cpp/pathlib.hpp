2772a598619810ea31030a98c3f211fddfba3bde
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-08-11 12:23:22 -0400

    add Path class

--- cpp/pathlib.hpp
+++ cpp/pathlib.hpp
@@ -9,8 +9,28 @@
 #define PATH_SEPERATOR "/"
 #endif // _WIN32
 
+#include <string> // std::string
+
 #ifndef MAX_PATH_SIZE
 #define MAX_PATH_SIZE _MAX_PATH
 #endif // MAX_PATH_SIZE
+
+class Path
+{
+    public:
+        Path();
+        Path(const std::string path);
+        bool exists();
+        Path absolute();
+        std::string to_string();
+        Path operator/(Path &child);
+        Path operator/(const std::string child);
+        bool is_relative();
+        Path &get_parent();
+        void mkdir();
+        void touch();
+    private:
+        std::string __path;
+};
 
 #endif // PATHLIB_HPP_
