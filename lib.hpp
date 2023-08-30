ba6583c1fc5b2b28887b9822ab7c8a6e0aaa0136
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-08-29 22:12:44 -0400

    Create lib.hpp

---
+++ lib.hpp
@@ -0,0 +1,10 @@
+#ifndef LIB_HPP_
+#define LIB_HPP_
+
+#define EXCPETION_IMPLEMENTATION
+#include "./cpp/exception.hpp"
+
+#define PATHLIB_IMPLEMENTATION
+#include "./cpp/pathlib.hpp"
+
+#endif // LIB_HPP_
