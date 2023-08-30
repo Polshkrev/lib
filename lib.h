f61390440bb364063718aa444de029f63f4d99e8
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-08-29 22:11:57 -0400

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,16 @@
+#ifndef LIB_H_
+#define LIB_H_
+
+#define STRINGS_IMPLEMENTATION
+#include "./c/strings.h"
+
+#define LOGGER_IMPLEMENTATION
+#include "./c/io/logger.h"
+
+#define FLAG_IMPLEMENTATION
+#include "./c/runtime/flag.h"
+
+#define PATH_IMPLEMENTATION
+#include "./path.h"
+
+#endif // LIB_H_
