4cd53c6035b3b9123b43d44ebe7fa5954c67803a
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-02-21 15:25:10 -0500

    return type fix of string_builder_at

--- c/string_builder.h
+++ c/string_builder.h
@@ -39,7 +39,7 @@ void string_builder_append(string_builder_t *builder, char item);
 * @returns The address of a character stored within the array at the given index.
 * @throws If the given index is outside of the size of the underlying array, then an `IndexError` is thrown and the programme exits.
 */
-char string_builder_at(const string_builder_t *builder, size_t index);
+char *string_builder_at(const string_builder_t *builder, size_t index);
 
 /*
 * @brief Access the underlying flattened array. This 'string' is not null-terminated unless explicitly appended to the array.
@@ -128,7 +128,7 @@ void string_builder_append(string_builder_t *builder, char item)
 * @returns The address of a character stored within the array at the given index.
 * @throws If the given index is outside of the size of the underlying array, then an `IndexError` is thrown and the programme exits.
 */
-char string_builder_at(const string_builder_t *builder, size_t index)
+char *string_builder_at(const string_builder_t *builder, size_t index)
 {
     if (index > builder->size)
     {
@@ -158,7 +158,7 @@ void string_builder_combine(string_builder_t *destination, const string_builder_
 {
     for (size_t i = 0; i < source->size; ++i)
     {
-        string_builder_append(destination, string_builder_at(source, i));
+        string_builder_append(destination, *string_builder_at(source, i));
     }
 }
 
