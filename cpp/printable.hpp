ac74e4df73b7626f34b80c36ceabe1fca7ba43bc
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-09-08 13:04:19 -0400

    Update printable.hpp

--- cpp/printable.hpp
+++ cpp/printable.hpp
@@ -1,5 +1,5 @@
-#ifndef PRINTABLE_HPP
-#define PRINTABLE_HPP
+#ifndef PRINTABLE_HPP_
+#define PRINTABLE_HPP_
 
 namespace polutils
 {
@@ -14,6 +14,28 @@ namespace polutils
          */
         virtual const char *to_string(void) const noexcept = 0;
     };
+
+    /**
+     * @brief Print a given printable.
+     * @param printable Printable to print.
+     */
+    void print(const printable_t &printable) noexcept;
 }
 
-#endif // PRINTABLE_HPP
+#endif // PRINTABLE_HPP_
+
+#ifdef PRINTABLE_IMPLEMENTATION
+
+namespace polutils
+{
+    /**
+     * @brief Print a given printable.
+     * @param printable Printable to print.
+     */
+    void print(const printable_t &printable) noexcept
+    {
+        std::printf("%s\n", printable.to_string());
+    }
+}
+
+#endif // PRINTABLE_IMPLEMENTATION
